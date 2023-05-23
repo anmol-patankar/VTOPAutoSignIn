22042a328bf4326284daacc3a97ac1a5cb39a19f
Nitrodium <anmolpatankar@gmail.com>
2023-05-23 21:14:29 +0530

    refactored page navigation and fetching

--- VTOP Signin.py
+++ VTOP Signin.py
@@ -5,36 +5,40 @@ import time
 import urllib
 from selenium.webdriver.common.keys import Keys
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-options.add_experimental_option("excludeSwitches", ['enable-automation'])
-driver = webdriver.Chrome(options=options)
+user=str(input())
+passwd=str(input())
 
-driver.get("https://vtop.vitbhopal.ac.in/vtop")
+#user="USERNAME_HERE"
+#passwd="PASSWORD_HERE"
 
-loginToVTOPButton=driver.find_element("xpath","//*[@id='page-wrapper']/div/div[1]/div[1]/div[3]/div/button")
-loginToVTOPButton.click()
+def BrowserSetup():
+    global driver
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    options.add_experimental_option("excludeSwitches", ['enable-automation'])
+    driver = webdriver.Chrome(options=options)
 
-user="USERNAME_HERE"
-passwd="PASSWORD_HERE"
-time.sleep(0.5)
-usernamefield=driver.find_element(By.CSS_SELECTOR, "input[name='uname'][type='text']")
-passwordfield=driver.find_element(By.CSS_SELECTOR, "input[name='passwd'][type='password']")
-usernamefield.send_keys(user)
-passwordfield.send_keys(passwd)
-captchaImageElement=driver.find_element(By.XPATH, "/html/body/div[1]/div/section/div/div[2]/form/div[3]/div/div/img")
-captchaSrc=captchaImageElement.get_attribute('src')
-urllib.request.urlretrieve(captchaSrc,"captcha.png")
-cropped_image=Image.open("captcha.png").convert("L")
-pixel_matrix = cropped_image.load()
-for col in range(0, cropped_image.height):
-    for row in range(0, cropped_image.width):
-        if pixel_matrix[row, col] != 0:
-            pixel_matrix[row, col] = 255
-cropped_image.save("captcha_clean.png")
-import imgprocess
-captchafield=driver.find_element(By.CSS_SELECTOR, "input[name='captchaCheck'][type='text']")
-captchafield.send_keys(imgprocess.captchaAns)
-signinButton=driver.find_element("xpath","//*[@id='captcha']")
-signinButton.click()
+def NavgiatePage():
+    driver.get("https://vtop.vitbhopal.ac.in/vtop")
+    loginToVTOPButton=driver.find_element("xpath","//*[@id='page-wrapper']/div/div[1]/div[1]/div[3]/div/button")
+    loginToVTOPButton.click()
+    time.sleep(0.5)
+    
+def FetchCaptcha():
+    captchaImageElement=driver.find_element(By.XPATH, "/html/body/div[1]/div/section/div/div[2]/form/div[3]/div/div/img")
+    captchaSrc=captchaImageElement.get_attribute('src')
+    urllib.request.urlretrieve(captchaSrc,"captcha.png")
+
+def InputSignIn():
+    import imgprocess
+    driver.find_element(By.CSS_SELECTOR, "input[name='uname'][type='text']").send_keys(user)
+    driver.find_element(By.CSS_SELECTOR, "input[name='passwd'][type='password']").send_keys(passwd)
+    driver.find_element(By.CSS_SELECTOR, "input[name='captchaCheck'][type='text']").send_keys(imgprocess.captchaAns)
+    driver.find_element("xpath","//*[@id='captcha']").click()
+    
+BrowserSetup()
+NavgiatePage()
+FetchCaptcha()
+InputSignIn()
+
 input()
--- imgprocess.py
+++ imgprocess.py
@@ -3,22 +3,33 @@ from Levenshtein import distance as levdist
 import time
 import json
 from difflib import SequenceMatcher
-im=Image.open("captcha_clean.png")
-rgbimg = Image.new("RGBA", im.size)
-rgbimg.paste(im)
-rgbimg.save('captcha_clean.png')
-im=Image.open("captcha_clean.png")
-cwidth,cheight=im.size
-top=0
-bottom=cheight
 captchaAns=""
 count=1
 left=0
-right=cwidth/6
-with open('captcha_bitmap.txt') as f:
-    data=f.read()
-alphanum=json.loads(data)
-def checkVal(stri):
+top=0
+
+def ImagePreprocess():
+    global cwidth,bottom,rgbimg
+    im=Image.open("captcha.png").convert("L")
+    pixel_matrix = im.load()
+    for col in range(0, im.height):
+        for row in range(0, im.width):
+            if pixel_matrix[row, col] != 0:
+                pixel_matrix[row, col] = 255
+    rgbimg = Image.new("RGBA", im.size)
+    rgbimg.paste(im)
+    rgbimg.save('captcha_clean.png')
+    cwidth,bottom=rgbimg.size
+    
+
+
+
+def LoadBitMap():
+    global alphanum
+    with open('captcha_bitmap.txt') as f:
+        data=f.read()
+    alphanum=json.loads(data)
+def EvaluateChar(stri):
     temp=-1
     currentmaxsim=2000
     for key in alphanum:
@@ -26,24 +37,30 @@ def checkVal(stri):
             currentmaxsim=levdist(stri,alphanum[key])
             temp=key
     return temp
-def Extract(lst):
+def FlattenExtract(lst):
     return [item[0] for item in lst]
-while right<=cwidth:
-    
-    bitmap_string=""
-    cropthing=(left, top+12, right, bottom-1)
-    cr_im=im.crop(cropthing)
-    pix_val = list(cr_im.getdata())
-    pix_val=Extract(pix_val)
-    for i in range(len(pix_val)):
-        if pix_val[i]==0:
-            bitmap_string+="0"
-        else:
-            bitmap_string+="1"
-    captchaAns+=str(checkVal(bitmap_string))
-    left=right
-    right+=cwidth/6
-def captchaSolve():
-    return captchaAns
+def CaptchaSolver():
+    global captchaAns
+    right=cwidth/6
+    left=0
+    while right<=cwidth:
+        bitmap_string=""
+        cropthing=(left, top+12, right, bottom-1)
+        cr_im=rgbimg.crop(cropthing)
+        pix_val = list(cr_im.getdata())
+        pix_val=FlattenExtract(pix_val)
+        for i in range(len(pix_val)):
+            if pix_val[i]==0:
+                bitmap_string+="0"
+            else:
+                bitmap_string+="1"
+        captchaAns+=str(EvaluateChar(bitmap_string))
+        left=right
+        right+=cwidth/6
+
+ImagePreprocess()
+LoadBitMap()
+CaptchaSolver()
+
 
 
